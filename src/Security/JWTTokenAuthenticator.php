8ee1e95ec1f838fb905952a71be01c64d3f585e9
Patrick Luca Fazzi <patrick91@live.it>
2019-03-18 23:20:33 +0100

    refactor: if token is corrupted or expired now user is redirected to login page

---
+++ src/Security/JWTTokenAuthenticator.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Security;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\InvalidTokenException;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\Guard\JWTTokenAuthenticator as BaseAuthenticator;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\TokenExtractor\TokenExtractorInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+
+class JWTTokenAuthenticator extends BaseAuthenticator
+{
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * JWTTokenAuthenticator constructor.
+     * @param RouterInterface $router
+     * @param JWTTokenManagerInterface $jwtManager
+     * @param EventDispatcherInterface $dispatcher
+     * @param TokenExtractorInterface $tokenExtractor
+     */
+    public function __construct(
+        RouterInterface $router,
+        JWTTokenManagerInterface $jwtManager,
+        EventDispatcherInterface $dispatcher,
+        TokenExtractorInterface $tokenExtractor
+    )
+    {
+        parent::__construct($jwtManager, $dispatcher, $tokenExtractor);
+
+        $this->router = $router;
+    }
+
+    public function supports(Request $request)
+    {
+        if(!parent::supports($request)) {
+            return false;
+        }
+
+        try {
+            parent::getCredentials($request);
+        } catch (InvalidTokenException $exception) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        return $this->redirectToLoginPage();
+    }
+
+    private function redirectToLoginPage(): Response
+    {
+        return new RedirectResponse($this->router->generate('web_login_check'));
+    }
+}
