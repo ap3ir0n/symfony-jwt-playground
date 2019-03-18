62758bd55d27e5fd5c8ce08a95e71c36d51c1cab
Patrick Luca Fazzi <patrick91@live.it>
2019-03-18 18:32:58 +0100

    feat: server and jwt bundle installed

---
+++ config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+];
